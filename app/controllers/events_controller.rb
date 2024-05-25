0a98f373859fea33bae59f260f1840b7ca052bf9
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-25 15:33:53 -0300

    feat: Add edit and destroy event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,26 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(article_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to "/users/#{params[current_user.id]}"
+  end
+
   private
 
   def article_params
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   validates :title, :local, :date, :creator, presence: true
   belongs_to :creator, class_name: "User"
-  has_many :participate_events, foreign_key: :event_id
-  has_many :participants, through: :participate_events, source: :user
+  has_many :participate_events, foreign_key: :event_id, dependent: :destroy
+  has_many :participants, through: :participate_events, source: :user, dependent: :destroy
 
   scope :upcoming_events, -> { where(date: (Time.now)..) }
   scope :past_events, -> { where(date: ..(Time.now)) }
