a1949a152bebce28e872ebda38a323457f04ee50
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-25 15:12:40 -0300

    feat: Add separate upcoming and past events in event index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :participate_events, foreign_key: :event_id
   has_many :participants, through: :participate_events, source: :user
+
+  def self.past_events
+    Event.where(date: ..(Time.now))
+  end
+
+  def self.upcoming_events
+    Event.where(date: (Time.now)..)
+  end
 end
